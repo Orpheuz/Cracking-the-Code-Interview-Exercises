95179f41f5ca0834f7274b192b792d919efb64f4
Vítor Teixeira <vitor_filipe_@hotmail.com>
2017-03-31 01:01:44 +0100

    Animal Shelter

--- src/com/vitorteixeira/AnimalShelter.java
+++ src/com/vitorteixeira/AnimalShelter.java
@@ -1,8 +1,104 @@
 package com.vitorteixeira;
 
+import java.util.NoSuchElementException;
+
 /**
  * Created by vitor on 30-03-2017.
  */
+
+abstract class Animal {
+    private int queueNumber;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQueueNumber() {
+        return queueNumber;
+    }
+
+    public void setQueueNumber(int queueNumber) {
+        this.queueNumber = queueNumber;
+    }
+
+    public boolean isOlder(Animal a) {
+        return this.queueNumber < a.getQueueNumber();
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+}
+
+class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+}
+
+class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name);
+    }
+}
+
 public class AnimalShelter {
 
+    private int order = 0;
+    private Node<Dog> dogHead = null;
+    private Node<Dog> dogTail = null;
+    private Node<Cat> catHead = null;
+    private Node<Cat> catTail = null;
+
+    public void enqueue(Animal a) {
+        a.setQueueNumber(order);
+        if (a instanceof Dog) {
+            if (dogHead == null) {
+                dogHead = new Node(a);
+                dogTail = dogHead;
+            } else {
+                dogTail.next = new Node(a);
+                dogTail = dogTail.next;
+            }
+        }
+        if (a instanceof Cat) {
+            if (catHead == null) {
+                catHead = new Node(a);
+                catTail = catHead;
+            } else {
+                catTail.next = new Node(a);
+                catTail = catTail.next;
+            }
+        }
+        order++;
+    }
+
+    public Dog dequeueDog() {
+        if(dogHead == null)
+            throw new NoSuchElementException();
+        Dog dog = dogHead.data;
+        dogHead = dogHead.next;
+        return dog;
+    }
+
+    public Cat dequeueCat() {
+        if(catHead == null)
+            throw new NoSuchElementException();
+        Cat cat = catHead.data;
+        catHead = catHead.next;
+        return cat;
+    }
+
+    public Animal dequeueAny() {
+        Dog dog = dogHead.data;
+        Cat cat = catHead.data;
+
+        if(dog.isOlder(cat))
+            return dequeueDog();
+        else return dequeueCat();
+    }
 }
--- src/com/vitorteixeira/Main.java
+++ src/com/vitorteixeira/Main.java
@@ -3,17 +3,24 @@ package com.vitorteixeira;
 public class Main {
 
     public static void main(String[] args) {
-        Stack<Integer> s1 = new Stack<>();
+        AnimalShelter animalShelter = new AnimalShelter();
 
-        s1.push(10);
-        s1.push(13);
-        s1.push(2);
+        Dog d1 = new Dog("Dog 1");
+        Cat c1 = new Cat("Cat 1");
+        Dog d2 = new Dog("Dog 2");
+        Cat c2 = new Cat("Cat 2");
+        Dog d3 = new Dog("Dog 3");
 
-        Stack.sortStack(s1);
+        animalShelter.enqueue(d1);
+        animalShelter.enqueue(c1);
+        animalShelter.enqueue(d2);
+        animalShelter.enqueue(c2);
+        animalShelter.enqueue(d3);
 
-        System.out.println(s1.pop());
-        System.out.println(s1.pop());
-        System.out.println(s1.pop());
-
+        System.out.println(animalShelter.dequeueAny().getName());
+        System.out.println(animalShelter.dequeueDog().getName());
+        System.out.println(animalShelter.dequeueCat().getName());
+        System.out.println(animalShelter.dequeueAny().getName());
+        System.out.println(animalShelter.dequeueDog().getName());
     }
 }
