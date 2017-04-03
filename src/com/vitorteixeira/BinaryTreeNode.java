6206fb8fef55b33f4381c569b3e52cdf6f770335
Vítor Teixeira <vitor_filipe_@hotmail.com>
2017-04-03 20:41:18 +0100

    Min Heap implementation without arrays

---
+++ src/com/vitorteixeira/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+package com.vitorteixeira;
+
+/**
+ * Created by vitor on 02-04-2017.
+ */
+public
+class BinaryTreeNode<T> {
+    public T data;
+    public BinaryTreeNode left;
+    public BinaryTreeNode right;
+}
